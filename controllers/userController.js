cf76bc656095a02fcd25f64b0ddd0220d4e9ea33
Mehmet Tokgöz <56408993+mehmettokgoz@users.noreply.github.com>
2021-02-12 23:07:37 +0300

    Register mechanism is created.

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,8 +9,15 @@ exports.logout = function() {
 }
 
 exports.register = function(req, res) {
-    let user = new User()
-    res.send("Thanks for trying to register.")
+    let user = new User(req.body)
+    user.register()
+    
+    if (user.errors.length == 0) {
+        res.send("Thanks")
+    } else {
+        res.send(user.errors)
+    }
+    
     
 }
 
--- models/User.js
+++ models/User.js
@@ -1,7 +1,62 @@
-let User = function(username,email, password){
-    this.username = username,
-    this.email = email,
-    this.password = password
+const validator = require("validator")
+const database = require('../db')
+const users = database.collection('users')
+
+let User = function(data){
+    
+    this.data = data
+    this.errors = []
 }
+
+User.prototype.validate = function() {
+
+    if (this.data.username == "") { this.errors.push("Username shouldn't be empty") }
+
+    if (this.data.email == "") { this.errors.push(" n                                                                                                                      pty") }
+
+    if (this.data.password == "") { this.errors.push("Password shouldn't be empty") }
+
+    if (this.data.password.length > 0 && this.data.password.length < 8) { this.errors.push("Password should be at least 8 character") }
+    
+    if (this.data.password.username > 0 && this.data.password.length < 3) { this.errors.push("Username should be at least 3 character") }
+
+    if (!validator.isEmail(this.data.email)) { this.errors.push("You must provide a valid email.")}
+
+    if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers")}
+}
+
+User.prototype.cleanUp = function() {
+
+    if (typeof(this.data.username) != "string") {this.data.username = ""}
+
+    if (typeof(this.data.email) != "string") {this.data.email = ""}
+
+    if (typeof(this.data.password) != "string") {this.data.password = ""}
+
+    // Get rid of any bogus properties
+
+    this.data = {
+        username: this.data.username.trim().toLowerCase(),
+        email: this.data.email.trim().toLowerCase(),
+        password: this.data.password
+    }
+
+}
+
+User.prototype.register = function() {
+
+    //Step 1 : Validate User Data
+    this.cleanUp()
+    this.validate()
+
+    
+    //Step 2 : If no error, Add user to database
+    if(!this.errors.length) {
+        users.insertOne(this.data)
+    }
+
+}
+
+
 
 module.exports = User
